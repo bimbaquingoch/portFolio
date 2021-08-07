18b175eea37e272c4db4f62a379ba76df520fc86
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-08-06 21:45:49 -0500

    home about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import { Home } from "./components/home/Home";
+import { Home } from "./components/home/Home";
 import { About } from "./components/about/About";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="main">
-      {/*<Home />*/}
+      <Home />
       {/*about section*/}
       <About />
     </div>
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import aboutImg from "../../media/about-img.png";
 import "./About.css";
 
+const Education = () => {
+  return (
+    <div className="tab-content" id="education">
+      <div className="timeline">
+        <div className="timeline-item">
+          <span className="date">2013-2016</span>
+          <h4>holaaaa</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+            dolores maxime repudiandae, molestias ratione iste praesentium
+            tempora nobis incidunt inventore exercitationem aut ab atque
+            adipisci expedita. Fuga nesciunt sit saepe?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Experience = () => {
+  return (
+    <div className="tab-content" id="experience">
+      <div className="timeline">
+        <div className="timeline-item">
+          <span className="date"></span>
+          <h4>
+            Experiencia <span>pagina random</span>
+          </h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
+            dignissimos ea amet nemo, delectus eaque?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const About = () => {
+  const [eduExp, setEduExp] = useState(false);
   const skils = [
     "html",
     "css",
@@ -18,6 +57,7 @@ export const About = () => {
     "NodeJS",
     "MongoDB",
   ];
+
   return (
     <section className="about-section sec-padding">
       <div className="container">
@@ -39,7 +79,7 @@ export const About = () => {
               mollitia aliquid libero eaque ab vitae quo repellendus, autem eum
               minus.
             </p>
-
+            {/*skills*/}
             <h3>Skills</h3>
             <div className="skills">
               {skils.map((item) => (
@@ -48,15 +88,41 @@ export const About = () => {
                 </div>
               ))}
             </div>
-
+            {/* education experience*/}
             <div className="about-tabs">
-              <button type="button" className="tab-item active" data-target="#">
+              <button
+                onClick={() => setEduExp(false)}
+                type="button"
+                className="tab-item"
+                data-target="#"
+                style={
+                  eduExp === false ? { color: "#ff005d" } : { color: "#18293c" }
+                }
+              >
                 Education
               </button>
-              <button type="button" className="tab-item" data-target="#">
+              <button
+                onClick={() => setEduExp(true)}
+                type="button"
+                className="tab-item"
+                data-target="#"
+                style={
+                  eduExp === true ? { color: "#ff005d" } : { color: "#18293c" }
+                }
+              >
                 Experience
               </button>
             </div>
+            {eduExp === false ? <Education /> : <Experience />}
+            {/* contact buttons*/}
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
+            <a href="#" className="btn">
+              download cv
+            </a>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
+            <a href="#" className="btn">
+              contact me
+            </a>
           </div>
         </div>
       </div>
