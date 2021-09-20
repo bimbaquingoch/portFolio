f5d89fc8f0b33f315f0e395f2b8f6dbf8af73177
bimbaquingoch <bryan_3871@hotmail.com>
2021-09-20 13:00:40 -0500

    styled components

--- src/components/contact/Info.js
+++ src/components/contact/Info.js
@@ -1,56 +1,99 @@
 import React from "react";
-import "./Contact.css";
+import styled from "styled-components";
 
-const social = [
-  {
-    id: 0,
-    alt: "github",
-    img: "https://img.icons8.com/fluency/48/000000/github.png",
-    url: "https://github.com/bimbaquingoch",
-  },
-  {
-    id: 1,
-    alt: "linkedin",
-    img: "https://img.icons8.com/color/48/000000/linkedin.png",
-    url: "https://www.linkedin.com/in/bryan-imbaquingo-almagro-636a90168/",
-  },
-  {
-    id: 2,
-    alt: "instagram",
-    img: "https://img.icons8.com/fluency/48/000000/instagram-new.png",
-    url: "https://www.instagram.com/bryandresimba/",
-  },
-  {
-    id: 3,
-    alt: "twitter",
-    img: "https://img.icons8.com/color/48/000000/twitter--v1.png",
-    url: "https://twitter.com/bryandresimba",
-  },
-];
+import {
+  WhatsApp,
+  Mail,
+  Instagram,
+  LinkedIn,
+  Twitter,
+  GitHub,
+} from "@material-ui/icons";
+
+//     url: "https://github.com/bimbaquingoch",
+//     url: "https://www.linkedin.com/in/bryan-imbaquingo-almagro-636a90168/",
+//     url: "https://www.instagram.com/bryandresimba/",
+//     url: "https://twitter.com/bryandresimba",
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-top: 80px;
+  width: 100%;
+`;
+
+const InfoItem = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SocialIcon = styled.div`
+  align-items: center;
+  background-color: #${({ color }) => color};
+  border-radius: 50%;
+  color: #fff;
+  cursor: pointer;
+  display: flex;
+  height: 40px;
+  justify-content: center;
+  margin-right: 20px;
+  transition: all 500ms ease;
+  width: 40px;
+
+  &:hover {
+    transform: scale(1.3);
+  }
+`;
+
+const ContactItem = styled.div`
+  align-items: center;
+  display: flex;
+  margin: 10px;
+`;
 
 export const Info = () => {
   return (
-    <div className="contact-info">
-      <div className="contact-info-item">
-        <h3>
-          email:<p>bryan_3871@hotmail.com</p>
-        </h3>
-      </div>
-      <div className="contact-info-item">
-        <h3>
-          phone:<p>+593 963 766 640</p>
-        </h3>
-      </div>
+    <Container>
+      <ContactItem>
+        <SocialIcon color="57c181">
+          <WhatsApp fontSize="large" />
+        </SocialIcon>
+        +593 9637 66640
+      </ContactItem>
 
-      <div className="contact-info-item">
-        <div className="social-links">
-          {social.map(({ img, url, alt, id }) => (
-            <a key={id} href={url} target="_blank" rel="noreferrer">
-              <img src={img} alt={alt} />
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
+      <ContactItem>
+        <SocialIcon color="3b5999">
+          <Mail fontSize="large" />
+        </SocialIcon>
+        bryanimbaquingo1@gmail.com
+      </ContactItem>
+
+      <InfoItem>
+        <SocialContainer>
+          <SocialIcon color="3b5999">
+            <LinkedIn fontSize="large" />
+          </SocialIcon>
+
+          <SocialIcon color="e4405f">
+            <Instagram fontSize="large" />
+          </SocialIcon>
+
+          <SocialIcon color="55acee">
+            <Twitter fontSize="large" />
+          </SocialIcon>
+
+          <SocialIcon color="051f2b">
+            <GitHub fontSize="large" />
+          </SocialIcon>
+        </SocialContainer>
+      </InfoItem>
+    </Container>
   );
 };
