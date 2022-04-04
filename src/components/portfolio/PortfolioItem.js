8a9cc7d3d3667bed2488f30bc62d585c018f1dee
bimbaquingoch <bryan_3871@hotmail.com>
2022-04-04 16:58:42 -0500

    update portfolio
    scroll on projects page

--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { projects } from "./data";
 
-export const PortfolioItem = ({ handleClick }) => {
+export const PortfolioItem = ( { handleClick } ) =>
+{
   return (
-    <div className="row">
-      {projects.map(({ title, img }, index) => (
-        <div key={`id ${index}`} className="portfolio-item">
+    <div className="row proyectos">
+      { projects.map( ( { title, img }, index ) => (
+        <div key={ `id ${ index }` } className="portfolio-item">
           <div className="portfolio-item-thumbnail">
-            <img src={img} alt={title} />
+            <img src={ img } alt={ title } />
           </div>
-          <h3 className="portfolio-item-title">{title}</h3>
+          <h3 className="portfolio-item-title">{ title }</h3>
           <button
-            onClick={() => handleClick(index)}
+            onClick={ () => handleClick( index ) }
             className="btn view-project-btn"
           >
             view project
           </button>
         </div>
-      ))}
+      ) ) }
     </div>
   );
 };
